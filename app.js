35e4470ee64f06bfb7af634ef70346871bb14abb
Roi Mekaiton <roimekaiton@gmail.com>
2023-12-26 00:09:00 +0200

    implement trash and completed buttons

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-List');
 
+
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+
 
 // Functions
 
@@ -40,4 +44,21 @@ function addTodo(event) {
 
     // Clear Todo INPUT VALUE
     todoInput.value = "";
+}
+
+
+function deleteCheck(e){
+    const item = e.target;
+
+    // DELETE TODO
+    if (item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    // CHECK MARK
+    if (item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
